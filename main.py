c14979457c1f6cc9b576e10c781a53a2784baf4c
Zichen Zhang <zhangzzc@umich.edu>
2024-06-11 14:38:39 -0400

    finish plot right

--- main.py
+++ main.py
@@ -75,9 +75,72 @@ def process_left(ChIA_Drop_old, left_anchor, right_anchor, region):
     print(valid_gems)
     return valid_gems
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from matplotlib.lines import Line2D
+def process_right(ChIA_Drop_old, left_anchor, right_anchor, region):
+    left_anchor_chrom = left_anchor.split('\t')[0]
+    left_anchor_start = int(left_anchor.split('\t')[1])
+    left_anchor_end = int(left_anchor.split('\t')[2])
+    right_anchor_end = int(right_anchor.split('\t')[2])
+
+    print("Filter GEMs to only include those in the region")
+    ChIA_Drop = ChIA_Drop_old.intersect(BedTool(region, from_string=True), wa=True, wb=True)
+
+    print("Create BedTool object for the right anchor")
+    right_anchor_bed = BedTool(right_anchor, from_string=True)
+
+    print("Get the GEM IDs that intersect with the right anchor")
+    intersecting_fragments = ChIA_Drop.intersect(right_anchor_bed, wa=True, wb=True)
+    intersecting_gem_ids = set(fragment.fields[4] for fragment in intersecting_fragments)
+    print("Filter ChIA_Drop to only include GEMs with intersecting IDs")
+    ChIA_Drop = ChIA_Drop.filter(lambda x: x.fields[4] in intersecting_gem_ids)
+
+    print("Filter ChIA_Drop to only include GEMs with the correct chrom id")
+    ChIA_Drop = ChIA_Drop.filter(lambda x: x.chrom == left_anchor_chrom)
+
+    print("Group GEM fragments by their GEM ID and get the min start and max end positions")
+    grouped_gems = {}
+    for fragment_interval in ChIA_Drop:
+        fragment = fragment_interval.fields
+        gem_id = fragment[4]
+        gem_size = int(fragment[3])
+        start = int(fragment[1])
+        end = int(fragment[2])
+
+        if gem_id not in grouped_gems.keys():
+            grouped_gems[gem_id] = {
+                'min_start': start,
+                'max_end': end,
+                'fragments': [fragment],
+                'gem_size': gem_size
+            }
+        else:
+            grouped_gems[gem_id]['min_start'] = min(grouped_gems[gem_id]['min_start'], start)
+            grouped_gems[gem_id]['max_end'] = max(grouped_gems[gem_id]['max_end'], end)
+            grouped_gems[gem_id]['fragments'].append(fragment)
+
+    print("Add valid gems")
+    valid_gems = []
+    for gem_id, gem_info in grouped_gems.items():
+        leftmost_fragment_start = gem_info['min_start']
+        rightmost_fragment_end = gem_info['max_end']
+        gem_size = gem_info['gem_size']
+        gem_length = rightmost_fragment_end - leftmost_fragment_start
+
+        if (
+            rightmost_fragment_end <= right_anchor_end
+            and leftmost_fragment_start >= left_anchor_start
+            and len(gem_info['fragments']) == gem_size
+        ):
+            # Get all fragments of the valid GEM
+            fragments = [
+                pybedtools.create_interval_from_list(fragment)
+                for fragment in gem_info['fragments']
+            ]
+            valid_gems.append((gem_id, fragments, gem_length))
+
+    print("Sort the valid GEMs by their length")
+    valid_gems.sort(key=lambda x: x[2])
+    print(valid_gems)
+    return valid_gems
 
 def plot_ranked_gems(ranked_gems, output_file, left_anchor, right_anchor):
     fig, ax = plt.subplots(figsize=(15, 18))
@@ -156,7 +219,7 @@ def main(path1, path2, type, output_file):
 
     elif type == "right":
         sort_start_time = time.time()
-        ranked_gems = process_right(ChIA_Drop, left_anchor, right_anchor)
+        ranked_gems = process_right(ChIA_Drop, left_anchor, right_anchor, region)
         print(f"It took {time.time() - sort_start_time} secs in total to sort the GEMs")
 
         plot_start_time = time.time()
