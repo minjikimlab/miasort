876e2f4ba89f8ca728656327003f0749288acce9
zichenzhang04 <zhangzzc@umich.edu>
2024-07-16 14:35:53 -0400

    revise all shell scripts

--- bin/greatlakes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Stop on errors
-set -Eeuo pipefail
-set -x
-
-python main.py --path1 GM12878-cohesin-pooled_comp_FDR_0.1_ALL_motifext4kbboth.region.PEanno --path2 test-greatlakes.bedte \
---type abc --graphs BtoA\;BtoC\;AtoC\;CtoA\;AandC\;Bcentered --numfrag 2 --out_dir greatlakes
--- bin/run.sh
+++ bin/run.sh
@@ -14,7 +14,7 @@ start_time=$(date +%s)
 # --type abc --graphs BtoA\;BtoC\;AtoB\;AtoC\;CtoA\;CtoB\;AandC\;Bcentered --numfrag_min 2  --anchor_options yes_complete --out_dir test_folder_11_indexed
 
 time python main.py --path1 GM12878-cohesin-pooled_comp_FDR_0.1_ALL_motifext4kbboth.region.PEanno --path2 test-july-1.bedte \
---type abc --graphs BtoA\;BtoC\;AtoB\;AtoC\;CtoA\;CtoB\;AandC\;Bcentered --numfrag_min 2  --anchor_options yes_complete --out_dir test_folder_11_pyranges
+--type abc --graphs BtoA\;BtoC\;AtoB\;AtoC\;CtoA\;CtoB\;AandC\;Bcentered --numfrag_min 2  --anchor_options yes_complete --out_dir test_folder
 
 # Record the end time
 end_time=$(date +%s)
