20f3daac9dcfbbde3644617e1568aceba0164029
zichenzhang04 <zhangzzc@umich.edu>
2024-08-09 15:30:28 -0400

    add hic gl testing scripts

---
+++ server-scripts/bedtools-hic-5-line.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name=bedtools_hic_5_line
+#SBATCH --mail-user=zhangzzc@umich.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=./logs/bedtools_hic_5.log
+#SBATCH --mem=50000m
+
+python main.py --path1 4DNFIACOTIGL.pairs.gz.ext250bp.g8000bp.region --path2 anchors-5.bedte \
+--type abc --graphs BtoA\;BtoC\;AtoB\;AtoC\;CtoA\;CtoB\;AandC\;Bcentered --numfrag_min 2  --anchor_options no --out_dir test_greatlakes_bedtools_hic_5
