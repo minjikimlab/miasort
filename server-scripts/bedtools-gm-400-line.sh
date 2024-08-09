5b4c10d971127253bbef37d88db26fa14f0b0463
zichenzhang04 <zhangzzc@umich.edu>
2024-08-09 15:06:22 -0400

    revise 400-line

--- server-scripts/bedtools-gm-400-line.sh
+++ server-scripts/bedtools-gm-400-line.sh
@@ -4,6 +4,7 @@
 #SBATCH --mail-user=zhangzzc@umich.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=./logs/bedtools_gm_400.log
+#SBATCH --mem=12000m
 
 python main.py --path1 GM12878-cohesin-pooled_comp_FDR_0.1_ALL_motifext4kbboth.region.PEanno --path2 anchors-400.bedte \
 --type abc --graphs BtoA\;BtoC\;AtoB\;AtoC\;CtoA\;CtoB\;AandC\;Bcentered --numfrag_min 2  --anchor_options no --out_dir test_greatlakes_bedtools_gm_400
