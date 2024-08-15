173eda97610f047e18ad9720eb5d1f466e7d3073
zichenzhang04 <zhangzzc@umich.edu>
2024-08-15 13:09:42 -0400

    add sprite server scripts

---
+++ server-scripts/bedtools-sprite-1-line.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=bedtools_sprite_1_line
+#SBATCH --mail-user=zhangzzc@umich.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=./logs/bedtools_sprite_1.log
+#SBATCH --mem=12000m
+#SBATCH --time=01-02:00:00
+
+python main.py --path1 GM12878-SPRITE.byChromosome.clusters --path2 anchors-1.bedte \
+--type abc --graphs BtoA\;BtoC\;AtoB\;AtoC\;CtoA\;CtoB\;AandC\;Bcentered --numfrag_min 2  --anchor_options no --out_dir test_greatlakes_bedtools_sprite_1
