30667e5dc03d264d908e8fb4eaed80e067c85d81
zichenzhang04 <zhangzzc@umich.edu>
2024-08-15 15:00:22 -0400

    add more test points

---
+++ server-scripts/bedtools-gm-150-line.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=bedtools_gm_150_line
+#SBATCH --mail-user=zhangzzc@umich.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=./logs/bedtools_gm_150.log
+#SBATCH --mem=12000m
+#SBATCH --time=01-02:00:00
+
+python main.py --path1 GM12878-cohesin-pooled_comp_FDR_0.1_ALL_motifext4kbboth.region.PEanno --path2 anchors-150.bedte \
+--type abc --graphs BtoA\;BtoC\;AtoB\;AtoC\;CtoA\;CtoB\;AandC\;Bcentered --numfrag_min 2  --anchor_options no --out_dir test_greatlakes_bedtools_gm_150
