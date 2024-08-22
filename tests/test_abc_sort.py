cafa1c24f8d41e32486181e58fa53d7c98f7280f
Zichen Zhang <zhangzzc@umich.edu>
2024-08-21 22:44:54 -0400

    revise test styles

--- tests/test_abc_sort.py
+++ tests/test_abc_sort.py
@@ -2,5 +2,8 @@
 
 import miasort
 
-miasort.abc_sort("./data/test_input.region", "./data/test_input.domains", "AtoC;CtoA;AandC;Bcentered;BtoA;BtoC",
-                 out_dir="./test_folder_syn_6000", anchor_option="yes_complete")
+miasort.abc_sort("./data/test_input.region",
+                 "./data/test_input.domains",
+                 "AtoC;CtoA;AandC;Bcentered;BtoA;BtoC",
+                 out_dir="./test_folder_syn_6000",
+                 anchor_option="yes_complete")
--- tests/test_multiple_sort.py
+++ tests/test_multiple_sort.py
@@ -2,5 +2,7 @@
 
 import miasort
 
-miasort.multiple_sort("./data/test_input.region", "./data/test_input_abc.domains",
-                 out_dir="./test_folder_syn_AandBandC_6000", anchor_option="yes_complete")
+miasort.multiple_sort("./data/test_input.region",
+                      "./data/test_input_abc.domains",
+                      out_dir="./test_folder_syn_AandBandC_6000",
+                      anchor_option="yes_complete")
