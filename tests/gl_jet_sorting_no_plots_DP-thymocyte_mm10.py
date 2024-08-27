95969fb9980da40acbc4d04841030686c93ced21
Zichen Zhang <zhangzzc@umich.edu>
2024-08-26 23:12:10 -0400

    add jet testing

--- tests/gl_jet_sorting_no_plots_DP-thymocyte_mm10.py
+++ tests/gl_jet_sorting_no_plots_DP-thymocyte_mm10.py
@@ -5,6 +5,7 @@ import miasort
 miasort.abc_sort("../../DP-thymocyte_Hi-C_GSE199059.mm10.complexes",
                  "../../DP-thymocyte-jets.bedte",
                  "Bcentered;BtoA;BtoC",
+                 plot=False,
                  colors="red;#FF0000;#525252",
                  out_dir="../DP-thymocyte_Hi-C_GSE199059-DP-thymocyte-jets",
                  anchor_option="yes_top")
