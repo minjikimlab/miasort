71f1f0040e1c4120e12386631b941b97a840e4d3
Zichen Zhang <zhangzzc@umich.edu>
2025-03-04 12:00:53 -0500

    enable local testing; add histogram-enabled tests

--- tests/test_unlimited_multiple_sort_histogram_enabled.py
+++ tests/test_unlimited_multiple_sort_histogram_enabled.py
@@ -11,6 +11,7 @@ import miasort
 miasort.unlimited_multiple_sort("./data/test_input.region",
                                 "chr3:100000-108000;chr3:150000-155000;chr3:300000-308000;chr3:420000-428000",
                                 "yes;no;yes;yes",
+                                histogram=True, # Enable histogram plotting
                                 out_dir="./test_folder_syn_multiple_6000",
                                 anchor_option="yes_complete",
                                 subplots_margins=(0.65, 0.15, 0.9))
